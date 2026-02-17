4902b26c127dfc418fccd7ffdd0aac6c1cf0be7c
Nahom <nahiyo86@gmail.com>
2026-02-18 02:14:38 +0300

    feat: create the Client struct, which represents a connected websocket client

---
+++ internal/client/client.go
@@ -0,0 +1,60 @@
+// internal/client/client.go
+// This file defines the Client struct, which represents a connected client in the Skribble backend.
+// It includes fields for the client's ID, name, WebSocket connection, and a channel for sending messages to the client. The Client struct also has methods for reading messages from the client and writing messages to the client.
+
+package client
+
+import (
+	"log"
+
+	"github.com/gorilla/websocket"
+)
+
+type Client struct {
+	ID   string
+	Name string
+
+	Conn *websocket.Conn
+
+	Send chan []byte // outbound messages
+
+	RoomID string
+}
+
+func NewClient(id, name string, conn *websocket.Conn) *Client {
+	return &Client{
+		ID:   id,
+		Name: name,
+		Conn: conn,
+		Send: make(chan []byte, 256),
+	}
+}
+
+func (c *Client) ReadPump(onMessage func([]byte), onClose func()) {
+	defer func() {
+		onClose()
+		c.Conn.Close()
+	}()
+
+	for {
+		_, message, err := c.Conn.ReadMessage()
+		if err != nil {
+			log.Println("read error:", err)
+			break
+		}
+
+		onMessage(message)
+	}
+}
+
+func (c *Client) WritePump() {
+	defer c.Conn.Close()
+
+	for message := range c.Send {
+		err := c.Conn.WriteMessage(websocket.TextMessage, message)
+		if err != nil {
+			log.Println("write error:", err)
+			break
+		}
+	}
+}
