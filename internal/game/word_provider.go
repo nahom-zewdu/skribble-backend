f8c51af67060f9efd4c2aa5d21faacb979e90612
Nahom <nahiyo86@gmail.com>
2026-02-27 13:41:08 +0300

    chore: return a random selection of n words from the provider list

--- internal/game/word_provider.go
+++ internal/game/word_provider.go
@@ -3,6 +3,47 @@
 // The WordProvider is responsible for providing a list of words that players can choose from during their turn.
 package game
 
+import (
+	"math/rand"
+	"time"
+)
+
 type WordProvider interface {
 	GenerateChoices(n int) []string
+}
+
+type StaticWordProvider struct {
+	words []string
+}
+
+func NewStaticWordProvider() *StaticWordProvider {
+	return &StaticWordProvider{
+		words: []string{
+			"apple", "car", "house", "dog",
+			"guitar", "tree", "pizza", "phone",
+			"mountain", "river", "sun", "computer",
+		},
+	}
+}
+
+// GenerateChoices returns a random selection of n words from the provider's list.
+func (p *StaticWordProvider) GenerateChoices(n int) []string {
+	if len(p.words) < n {
+		return p.words
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
+	result := make([]string, 0, n)
+	used := make(map[int]bool)
+
+	for len(result) < n {
+		i := rand.Intn(len(p.words))
+		if !used[i] {
+			used[i] = true
+			result = append(result, p.words[i])
+		}
+	}
+
+	return result
 }
