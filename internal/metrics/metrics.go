afbf791a851e0fbe07b5a4fa17d7bd54294b69ca
Nahom <nahiyo86@gmail.com>
2026-06-22 18:16:09 +0300

    chore: make go return camelCase payloads for consistency with frontend

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -9,17 +9,17 @@ import (
 
 // Metrics holds various metrics for the Skribble backend, including active connections, rooms, messages, and bytes transferred. It uses atomic operations to ensure thread safety when updating these metrics.
 type Metrics struct {
-	ActiveConnections int64
-	PeakConnections   int64
+	ActiveConnections int64 `json:"activeConnections"`
+	PeakConnections   int64 `json:"peakConnections"`
 
-	ActiveRooms int64
-	PeakRooms   int64
+	ActiveRooms int64 `json:"activeRooms"`
+	PeakRooms   int64 `json:"peakRooms"`
 
-	TotalMessages int64
-	TotalBytes    int64
+	TotalMessages int64 `json:"totalMessages"`
+	TotalBytes    int64 `json:"totalBytes"`
 
-	DrawMessages int64
-	ChatMessages int64
+	DrawMessages int64 `json:"drawMessages"`
+	ChatMessages int64 `json:"chatMessages"`
 }
 
 var M = &Metrics{}
