4a9a19e5dc328dd541a10ba5c08366fd65f917bb
Nahom <nahiyo86@gmail.com>
2026-06-22 14:24:52 +0300

    chore: add loop to flush the current metrics to redis every 15s

--- internal/metrics/persistence.go
+++ internal/metrics/persistence.go
@@ -5,6 +5,7 @@ package metrics
 
 import (
 	"context"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -26,4 +27,17 @@ func InitRedis(
 	)
 
 	go flushLoop()
+}
+
+// flushLoop runs an infinite loop that flushes the current metrics to Redis every 15 seconds. It uses a ticker to trigger the flush operation at regular intervals. If the Redis client is not initialized, the Flush function will simply return without doing anything.
+func flushLoop() {
+	ticker := time.NewTicker(
+		15 * time.Second,
+	)
+
+	defer ticker.Stop()
+
+	for range ticker.C {
+		Flush()
+	}
 }
